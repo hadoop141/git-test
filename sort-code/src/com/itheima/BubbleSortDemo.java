a447a54647d5915c02c310962907ceb45850c850
ccc <ccc@atguigu.com>
2022-12-15 17:41:12 +0800

    冒泡，选择排序

---
+++ sort-code/src/com/itheima/BubbleSortDemo.java
@@ -0,0 +1,44 @@
+package com.itheima;
+
+
+import java.util.Arrays;
+
+public class BubbleSortDemo {
+    public static void main(String[] args) {
+         /*
+            冒泡排序：
+            核心思想：
+            1，相邻的元素两两比较，大的放右边，小的放左边。
+            2，第一轮比较完毕之后，最大值就已经确定，第二轮可以少循环一次，后面以此类推。
+            3，如果数组中有n个数据，总共我们只要执行n-1轮的代码就可以。
+        */
+        int[] arr = {2, 4, 5, 3, 1};
+
+        //外循环 总共要比较 arr.length-1 趟
+        for (int i = 1; i <= arr.length - 1; i++) {
+
+            // 一开始 {2, 4, 5, 3, 1} 两两比较要比较 四次
+            //内循环 第一次要比较 arr.leng -1次   经过一次循环变成了  {2, 4, 3, 1, 5}
+            //内循环 第二次要比较 arr.leng -2次   比较3次 前四个数比较，最后一个数不比较
+            // ..... 最后一次要比较 arr.length - (arr.length-1)
+            //也就是剩下两个数 比较一次
+
+            for (int j = 0; j < arr.length - i; j++) {
+                //-1 : 为了防止索引越界 arr[j] arr[j+1]
+                //如果前一个数 > 后一个数 就交换位置
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+
+
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+
+}
---
+++ sort-code/src/com/itheima/SelectionDemo.java
@@ -0,0 +1,35 @@
+package com.itheima;
+
+import java.util.Arrays;
+
+public class SelectionDemo {
+    public static void main(String[] args) {
+         /*
+            选择排序：
+                1，从0索引开始，跟后面的元素一一比较。
+                2，小的放前面，大的放后面。
+                3，第一次循环结束后，最小的数据已经确定。
+                4，第二次循环从1索引开始以此类推。
+
+         */
+        int[] arr = {2, 4, 5, 3, 1, 6};
+
+        //外循环 循环 arr.length-1 次
+        //i ： 每次拿着数组中那个数据 arr[i] 和 后面的数据作比较
+        for (int i = 0; i < arr.length-1; i++) {
+            //内循环：每一轮我要干什么事情？
+            //内循环 ：每次把数组中i索引的值arr[i]和后面的值比较，把小的放在前面
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[j] < arr[i]){ //如果后面的值 比arr[i] 小 就让他们交换位置，再继续和后面的值比较
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+
+    }
+}
